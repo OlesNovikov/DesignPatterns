2bcb5fad58d84d2c19a95d0ddcdd455c77806609
Oles <lxnvkv@gmail.com>
2022-08-01 12:09:38 +0300

    fix: added serialQueue to protect data

--- #1 Singleton/#2 Concurrency issue/AppSettingsDemo/AppSettings.swift
+++ #1 Singleton/#2 Concurrency issue/AppSettingsDemo/AppSettings.swift
@@ -12,21 +12,33 @@ final public class AppSettings {
     
     public static let shared = AppSettings()
     
+    private let serialQueue = DispatchQueue(label: "serialQueue")
+    
     private var settings: [String: Any] = ["Theme": "Dark",
                                            "MaxConcurrentDownloads": 4]
     
     private init() {}
     
     public func string(forKey key: String) -> String? {
-        return settings[key] as? String
+        var result: String?
+        serialQueue.sync {
+            result = settings[key] as? String
+        }
+        return result
     }
     
     public func int(forKey key: String) -> Int? {
-        return settings[key] as? Int
+        var result: Int?
+        serialQueue.sync {
+            result = settings[key] as? Int
+        }
+        return result
     }
     
     public func set(value: Any, forKey key: String) {
-        settings[key] = value
+        serialQueue.sync {
+            settings[key] = value
+        }
     }
     
 }
