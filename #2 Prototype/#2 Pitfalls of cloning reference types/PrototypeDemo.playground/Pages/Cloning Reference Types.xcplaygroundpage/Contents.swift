a06aa2df520fc85910a1806ae6dfc6b83aa25e54
Oles <lxnvkv@gmail.com>
2022-08-01 13:12:36 +0300

    feat: added class NameClass

---
+++ #2 Prototype/#2 Pitfalls of cloning reference types/PrototypeDemo.playground/Pages/Cloning Reference Types.xcplaygroundpage/Contents.swift
@@ -0,0 +1,28 @@
+class NameClass {
+    var firstName: String
+    var lastName: String
+    
+    init(firstName: String, lastName: String) {
+        self.firstName = firstName
+        self.lastName = lastName
+    }
+}
+
+extension NameClass: CustomStringConvertible {
+    public var description: String {
+        return "NameClass(firstName: \(firstName), lastName: \(lastName))"
+    }
+}
+
+
+var steve = NameClass(firstName: "Steve", lastName: "Johnson")
+var john = steve
+
+print(steve)
+print(john)
+
+john.firstName = "John"
+john.lastName = "Wallace"
+
+print(steve)
+print(john)
